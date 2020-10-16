14720f64ec9bc398d49ebb9737ec59efef3d5638
wsong2 <weixus@hotmail.com>
2020-10-16 11:21:57 +0100

    work save

---
+++ reactjs/tutorial/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+ entry: {
+	//pageOne: './src/main',
+	main: './src/main.js'
+ },
+ module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(css)$/,
+        exclude: /node_modules/,
+        use: ['css-loader']
+      }
+    ]
+ },
+ output: {
+    path: path.join(__dirname, '/dist'),
+    filename: '[name].js'
+ }
+};
