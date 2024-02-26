37da71ae555a05eeb45fa5d3252198b205246edf
w Song <weixus@hotmail.com>
2024-02-26 10:28:47 +0000

    ATT context of delete operation

--- restsvr/db.js
+++ restsvr/db.js
@@ -1,13 +1,11 @@
-//import { toISODateTime, toISOTimeStamp } from "./controllers/dateformat.js";
 import { MongoClient } from "mongodb";
 
 const uri = "mongodb://localhost:27017";
 const client = new MongoClient(uri);
 
-const chrOp = 'U';
+const chrOp = 'D';
 
 if (chrOp === 'I') {
-
     async function run() {
         try {
             const database = client.db("prolog");
@@ -24,27 +22,49 @@ if (chrOp === 'I') {
     }
     run().catch(console.dir);
 
-    exit(0);
+    process.exit();
 }
 
+if (chrOp === 'D') {
+    console.log('Do delete');
 
-async function run() {
-    try {
-        const database = client.db("prolog");
-        const simCounter = database.collection("simCounter");
-        // const updateDoc = {
-        //     { $set : { title: "SIM next Id"} },
-        //     { $inc : { count: 1} }
-        // };
-        //.const result = await simCounter.updateOne(updateDoc, options);
-        const filter = { title: "SIM next Id" };
-        const options = { upsert: false };
-        const result = await simCounter.findOneAndUpdate(filter, {$inc : { count: 1}}, options);
-        console.log(result); 
-
-        simCounter.find();
-    } finally {
-        await client.close();
+    async function run() {
+        try {
+            const database = client.db("prolog");
+            const simItems = database.collection("simItems");
+            const query = {simName: "BT rt-2"};
+            const result = await simItems.deleteOne(query);
+            console.log(`Result: ${result.deletedCount}`);
+        } finally {
+            await client.close();
+        }
     }
+    run().catch(console.dir);
+
+
+    //process.exit(); 
 }
-run().catch(console.dir);
+
+// process.exit();
+
+
+// async function run() {
+//     try {
+//         const database = client.db("prolog");
+//         const simCounter = database.collection("simCounter");
+//         // const updateDoc = {
+//         //     { $set : { title: "SIM next Id"} },
+//         //     { $inc : { count: 1} }
+//         // };
+//         //.const result = await simCounter.updateOne(updateDoc, options);
+//         const filter = { title: "SIM next Id" };
+//         const options = { upsert: false };
+//         const result = await simCounter.findOneAndUpdate(filter, {$inc : { count: 1}}, options);
+//         console.log(result); 
+
+//         simCounter.find();
+//     } finally {
+//         await client.close();
+//     }
+// }
+// run().catch(console.dir);
