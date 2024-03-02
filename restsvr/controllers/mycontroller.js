9d6075b7b770430a54d65b7c7334aa1fa742a896
w Song <weixus@hotmail.com>
2024-03-02 10:51:25 +0000

    gridN.xml instead of gridN.json

--- restsvr/controllers/mycontroller.js
+++ restsvr/controllers/mycontroller.js
@@ -6,8 +6,6 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-var mRecId = 1001;
-
 var htmP1, htmP2;
 
 function init()
